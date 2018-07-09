48b80c6c13b255fc1a2223d90d415dd83f2a2100
Jungtaek Lim <kabhwan@gmail.com>
2018-07-09 13:44:40 +0900

    Add app for benchmarking aggregation - write query status to file (no relevant to iot truck though)

---
+++ src/main/scala/com/hortonworks/spark/utils/QueryListenerWriteProgressToFile.scala
@@ -0,0 +1,31 @@
+package com.hortonworks.spark.utils
+
+import java.io.{File, PrintWriter}
+
+import org.apache.commons.logging.LogFactory
+import org.apache.spark.sql.streaming.StreamingQueryListener
+
+class QueryListenerWriteProgressToFile(brokerServers: String, queryStatusFile: String) extends StreamingQueryListener {
+  val logger = LogFactory.getLog(classOf[QueryListenerWriteProgressToFile].getName)
+  val writer = new PrintWriter(new File(queryStatusFile))
+
+  override def onQueryStarted(event: StreamingQueryListener.QueryStartedEvent): Unit = {
+    logger.info(s"Query is started for ID ${event.id} and RUNID ${event.runId}")
+  }
+
+  override def onQueryProgress(event: StreamingQueryListener.QueryProgressEvent): Unit = {
+
+    try {
+      writer.write(event.progress.json + "\n")
+      writer.flush()
+    } catch {
+      case e: Exception =>
+        logger.error("Error write event[" + event.progress.json + "] to file", e)
+    }
+  }
+
+  override def onQueryTerminated(event: StreamingQueryListener.QueryTerminatedEvent): Unit = {
+    logger.info(s"Query is terminated for ID ${event.id} and RUNID ${event.runId}")
+    writer.close()
+  }
+}
